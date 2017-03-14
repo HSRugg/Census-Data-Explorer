683dc91866a5839b34f972a0bf10e1c9b0905972
HSRugg <HSRugg@yahoo.com>
2017-03-13 21:16:05 -0700

    update for style
    set hight of header to 0 on sight change

--- change_title.js
+++ change_title.js
@@ -3,5 +3,6 @@ var changeTitle = function(state) {
 document.getElementById('my_p').innerHTML = "Displaying Date For "+state+", Pie Chart Data From 2012"
 
 document.getElementById('tooltip').style.visibility = "hidden";
-document.getElementById('header').style.visibility = "none";
+document.getElementById('header').style.visibility = "hidden";
+document.getElementById('header').style.height = "0px";
 };
