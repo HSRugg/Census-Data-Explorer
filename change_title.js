a3e98402e232bae6e6c1f9af6b6e2ed8d9b6adeb
HSRugg <HSRugg@yahoo.com>
2017-03-13 21:01:19 -0700

    remove header
    add line to hide the header

--- change_title.js
+++ change_title.js
@@ -3,4 +3,5 @@ var changeTitle = function(state) {
 document.getElementById('my_p').innerHTML = "Displaying Date For "+state+", Pie Chart Data From 2012"
 
 document.getElementById('tooltip').style.visibility = "hidden";
+document.getElementById('header').style.visibility = "hidden";
 };
