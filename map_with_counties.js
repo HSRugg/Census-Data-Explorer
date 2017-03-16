b90191979e870573525f330fb77b12beeab8898c
Harrison <HSRugg@yahoo.com>
2017-03-16 16:48:07 -0700

    preping to add new map page

--- map_with_counties.js
+++ map_with_counties.js
@@ -1,8 +1,8 @@
-var path_to_folder = "/Census-Data-Explorer/data_by_state/"
-
+var path_to_folder = "/Census-Data-Explorer/data_for_US/"
+industry = "Construction"
 var make_inds_map_with_counties = function() {
     var year = "2012";
-    var path_to_data = path_to_folder+state+"_small_all_eds.csv";
+    var path_to_data = path_to_folder+industry+"_county_emp.csv";
         d3.csv(path_to_data, function(data) {
 
 
@@ -10,7 +10,9 @@ var make_inds_map_with_counties = function() {
     
     
     
-    console.log("hi")
+    console.log("hi");
+    console.log(data[0]);
+
 });
 }
 
