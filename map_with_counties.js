300acb6bcf1e6e66f2c233846dcb22826f5eddfd
Harrison <HSRugg@yahoo.com>
2017-03-16 16:44:46 -0700

    preping to add new map page

--- map_with_counties.js
+++ map_with_counties.js
@@ -1,6 +1,6 @@
 var path_to_folder = "/Census-Data-Explorer/data_by_state/"
 
-var make_inds_map_with_counties function() {
+var make_inds_map_with_counties = function() {
     var year = "2012";
     var path_to_data = path_to_folder+state+"_small_all_eds.csv";
         d3.csv(path_to_data, function(data) {
@@ -15,4 +15,4 @@ var make_inds_map_with_counties function() {
 }
 
 
-//make_inds_map_with_counties()
+make_inds_map_with_counties()
