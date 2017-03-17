653afc65309f0a17ad41744810f34fd10910a75c
Harrison <HSRugg@yahoo.com>
2017-03-16 17:19:42 -0700

    add map plot

--- map_with_counties.js
+++ map_with_counties.js
@@ -1,18 +1,51 @@
 
 var make_inds_map_with_counties = function(industry) {
     var path_to_folder = "/Census-Data-Explorer/data_for_US/"
-
-    var year = "2012";
+//    var year = "2012";
     var path_to_data = path_to_folder+industry+"_county_emp.csv";
-        d3.csv(path_to_data, function(data) {
-
-
-
-    
-    
-    
+        
     console.log("hi");
     console.log(data[0]);
+                        
+    var svg = d3.select("svg"),
+        width = +svg.attr("width"),
+        height = +svg.attr("height");
 
+    var my_map = d3.map();
+
+    var path = d3.geoPath();
+
+    var color = d3.scaleLinear()
+        .domain([0, 5115])
+        .range(["#BBDEFB", "darkblue"]);
+
+
+    d3.queue()
+        .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+        .defer(d3.csv(path_to_data, function(data) { my_map.set(d.id, +d.rate); })
+        .await(ready);
+            
+    console.log(my_map)
+    
+    function ready(error, us) {
+      if (error) throw error;
+
+      svg.append("g")
+          .attr("class", "counties")
+        .selectAll("path")
+        .data(topojson.feature(us, us.objects.counties).features)
+        .enter().append("path")
+          .attr("fill", function(d) { console.log(d.id); return color(my_map.get(d.id)); })
+          .attr("d", path)
+        .append("title")
+          .text(function(d) { return d.rate + "%"; });
+
+      svg.append("path")
+          .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+          .attr("class", "states")
+          .attr("d", path);
+            
+            
+            
 });
 }
