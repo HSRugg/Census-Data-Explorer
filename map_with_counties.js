4849c54c6de334a2e76795be17c6dc194eef0174
Harrison <HSRugg@yahoo.com>
2017-03-20 12:41:33 -0700

    change scale for map

--- map_with_counties.js
+++ map_with_counties.js
@@ -1,67 +1,151 @@
-local_path = ""
-console.log("hello")
-//local_path = "/censues_page"
+
 var make_inds_map_with_counties = function(industry) {
-//    d3.select("svg").remove();
-    d3.selectAll("svg > *").remove()
-    document.getElementById('my_p').innerHTML = "Displaying US "+industry+" Data From 2012"
-    d3.selectAll("#header").remove()
-    document.getElementById('tooltip').style.visibility = "hidden";
-//    document.getElementById('header').style.visibility = "hidden";
-    
-    
-    var path_to_folder = local_path+"/Census-Data-Explorer/data_for_US/"
-//    var year = "2012";
-    var path_to_data = path_to_folder+industry+"_county_emp.csv";
-    
-    
-    
-    var new_svg = d3.select("center").append("svg");
-    var svg = d3.select("svg"),
-         width = 1000,
-        height = 600;
 
-//        width = +svg.attr("width"),
-//        height = +svg.attr("height");
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height");
 
-    var my_map = d3.map();
+var my_map = d3.map();
+var name_map = d3.map();
+var map_2011 = d3.map();
 
-    var path = d3.geoPath();
-    console.log("path to data: " + path_to_data);
+var path = d3.geoPath();
+
+//var x = d3.scaleLinear()
+//    .domain([1, 10])
+//    .rangeRound([600, 860]);
+
+var color = d3.scaleLog()
+    .base(Math.E)
+    .domain([Math.exp(0), Math.exp(10)])
+    .range(["#BBDEFB", "darkblue"]);
+
+var g = svg.append("g")
+    .attr("class", "key")
+    .attr("transform", "translate(600,40)");
+
+g.selectAll("rect")
+  .data([Math.exp(0), Math.exp(2), Math.exp(4), Math.exp(6), Math.exp(8), Math.exp(10), Math.exp(12)])
+    .enter()
+    .append("rect")
+    .attr("height", 8)
+    .attr("x", function(d, i) { return (i*35); })
+    .attr("width", 34)
+    .attr("fill", function(d) { return color(d); })
+    .attr("text", function(d) { return d});
     
-    var color = d3.scaleLog()
-        .domain([0, 30115])
-        .range(["#BBDEFB", "darkblue"]);
 
-    
-    
+g.append("text")
+    .attr("class", "caption")
+    .attr("x", function(d, i) { return (i*40); })
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Log Scaled Employment Rate");
 
-    d3.queue()
-        .defer(d3.json, "https://d3js.org/us-10m.v1.json")
-        .defer(d3.csv, path_to_data, function(d) { my_map.set(d.id, +d.rate); })
-        .await(ready);
-                
+g.selectAll("line")
+  .data([Math.exp(0), Math.exp(2), Math.exp(4), Math.exp(6), Math.exp(8), Math.exp(10), Math.exp(12)])
+    .enter()
+    .append("line")
+    .style("stroke", "black")  
+    .attr("x1", function(d, i) { return (i*35+10); })     
+    .attr("y1", 30)      
+    .attr("x2", function(d, i) { return (i*35+17); })    
+    .attr("y2", 30);   
+
+var superscript = "⁰¹²³⁴⁵⁶⁷⁸⁹",
+    formatPower = function(d) { return (d + "").split("").map(function(c) { return superscript[c]; })}
+
+var keyLaybels = d3.select(".key").selectAll("keyLaybels")
+        .data([Math.exp(0), Math.exp(2), Math.exp(4), Math.exp(6), Math.exp(8), Math.exp(10), Math.exp(12)])
+        .enter()
+        .append("text")
+        .style('text-anchor','start')
+        .attr('transform', function(d,i,j) { return 'translate(14,0)' });
+    
+keyLaybels.attr("class", "value")
+              .attr("x", function(d, i) { return i * 35 - 20})
+              .attr("y", 30)
+              .attr("dx", -3)
+              .attr("dy", ".35em")
+              .attr("text-anchor", "end")
+              .text(function(d) { return ("e" + formatPower(Math.round(Math.log(d)))).replace(",",""); });
+    
+defs = svg.append("defs")
+
+		defs.append("marker")
+                .data([Math.exp(0), Math.exp(2), Math.exp(4), Math.exp(6), Math.exp(8), Math.exp(10), Math.exp(12)])
+                .enter()
+				.append("path")
+					.attr("d", "M0,-5L10,0L0,5")
+					.attr("class","arrowHead")
+                    .attr({
+					"id":"arrow",
+					"viewBox":"0 -5 10 10",
+					"refX":5,
+					"refY":0,
+					"markerWidth":5,
+					"markerHeight":5,
+					"orient":"auto"
+				});
+
+
+		d3.range(1).forEach(function(){ //I don't normally do loops like this, just a whim
+			svg.append('line')
+				.attr({
+					"class":"arrow",
+					"marker-end":"url(#arrow)",
+					"x1":width/2,
+					"y1":height/2,
+					"x2":width-10*2,
+					"y2":height/2
+				});
+		})
+				
+
+		
+d3.queue()
+    .defer(d3.json, "https://d3js.org/us-10m.v1.json")
+    .defer(d3.csv, "/Census-Data-Explorer/data_for_US/Construction_county_emp.csv", function(d) { my_map.set(d.id, +d.rate), name_map.set(d.id, d.name); } )
+    .await(ready);
+    
     function ready(error, us) {
-        console.log(error);
       if (error) throw error;
         
-      svg.append("g")
-          .attr("class", "counties")
-        .selectAll("path")
-        .data(topojson.feature(us, us.objects.counties).features)
-        .enter().append("path")
-          .attr("fill", function(d) { console.log(d.id); return color(my_map.get(d.id)); })
-          .attr("d", path)
-        .append("title")
-          .text(function(d) { return d.id + "%"; });
 
-      svg.append("path")
-          .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-          .attr("class", "states")
-          .attr("d", path);
-            
-            
-            
-};
+var missing_arr = [];
+
+var missing_fliter = function(d) { 
+            d.forEach(function(element) {
+                if(typeof(my_map.get(element.id)) == 'undefined')
+                { missing_arr.push(element.id) } 
+                })
+ };
+console.log(missing_arr);  
+        
+
+           
+  missing_fliter(topojson.feature(us, us.objects.counties).features);
+  svg.append("g")
+      .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+      .attr("fill", function(d) { return color(my_map.get(d.id))})
+      .attr("d", path)
+    .append("title")
+      .text(function(d) { return name_map.get(d.id); });
+
+  svg.append("path")
+      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+      .attr("class", "states")
+      .attr("d", path);
+        
+//  svg.selectAll("path").transition()
+//        .duration(1500)
+//        .attr("fill", function(d) { return color(map_2011.get(d.id)); });
+
+}
 }
 //make_inds_map_with_counties("Construction")
