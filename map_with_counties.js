9edb3c14156d9cb95631e227058b18606d401a65
Harrison <HSRugg@yahoo.com>
2017-03-20 13:31:11 -0700

    remove old map

--- map_with_counties.js
+++ map_with_counties.js
@@ -4,7 +4,9 @@
 var make_inds_map_with_counties = function(industry) {
 
     try {
-        d3.select('svg').remove()    
+        d3.select('svg').remove()
+        document.getElementById('tooltip').style.visibility = "hidden";
+        document.getElementById('header').remove()
 }
 catch(err) {
    //pass
