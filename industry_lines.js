1478b335fd10041b24849b318c70b715a386dd09
Harrison <HSRugg@yahoo.com>
2017-03-21 23:26:06 -0700

    fixes to css

--- industry_lines.js
+++ industry_lines.js
@@ -124,7 +124,8 @@ var make_inds_lines = function (state) {
         paths.exit().remove();
 
         paths.attr("d", function (d){ return line(d.values); })
-            .attr("stroke", function (d){ return colorScale(d.key); });
+            .attr("stroke", function (d){ return colorScale(d.key); })
+            .style("fill", "none");
     //        colorLegendG.call(colorLegend);
             
     }
